6e2065aa92023fbd5927e3d0a278bc5b1c3bbaa5
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-17 08:24:29 +0000

    add all

--- index.php
+++ index.php
@@ -37,8 +37,44 @@ echo "date 2 :".$date2."<br>";
 echo "date 3 :".$date3."<br>";
 echo "date 4 :".$date4."<br>";
 echo "date 5 :".$date5."<br>";
-echo "date 6 : l'année est-elle bissextile ? ".$responds."<br>";
+echo "date 6 : l'année est-elle bissextile ? ".$responds."<br><br>";
 ?>
 
+<!--======================== getdate() =======================-->   
+     
+<?php
+echo print_r(getdate())."<br><br>";
+?> 
+
+<!--======================= strtotime() ======================-->   
+
+
+<?php
+echo strtotime("09 June 1992")."<br><br>";
+?> 
+
+<!--======================= checkdate() ======================-->   
+    
+<?php
+
+var_dump(checkdate(2,29 , 2023))."<br>";
+var_dump(checkdate(2, 29, 2024))."<br>";
+
+?> 
+
+<!--======================= strtotime() ======================-->   
+
+<?php
+
+$Date = "2023-03-27";
+echo date('Y-m-d', strtotime($Date. ' + 1 days'))."|";
+echo date('Y-m-d', strtotime($Date. ' + 1 months'));
+
+?> 
+
+
+  
+  
+  
   </body>
 </html>
